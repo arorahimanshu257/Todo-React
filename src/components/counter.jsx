fdafd75e62582cee51c93906ff3bb28a9abd8c64
Himanshu Arora <38257545+arorahimanshu257@users.noreply.github.com>
2021-05-02 19:07:38 +0530

    Add files via upload

---
+++ src/components/counter.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.counter.value,
+  };
+
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+  //     return (
+  //       <ul>
+  //         {this.state.tags.map((tag) => (
+  //           <li key={tag}>{tag}</li>
+  //         ))}
+  //       </ul>
+  //     );
+  // }
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        {/* {this.state.tags.length === 0 && "Please create a new tag !"}
+        {this.renderTags()} */}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCounter() {
+    const { value: count } = this.props.counter;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+
+// const element = <h1>Hello World !</h1>;
+
+ReactDom.render(<App />, document.getElementById("root"));
